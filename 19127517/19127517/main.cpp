829c71839152fdb4dc86ad9d39a7fee19c465895
jason11501 <55671823+jason11501@users.noreply.github.com>
2022-07-08 08:50:49 +0700

    Update main.cpp

--- 19127517/19127517/main.cpp
+++ 19127517/19127517/main.cpp
@@ -344,7 +344,7 @@ void BuildTree(BSTNode* &root, int n)
     }
 }
 
-BSTNode* searchIDAndUpdate(BSTNode* t, string id)
+BSTNode* searchIDAndUpdate(BSTNode* &t, string id)
 {
     if (t == NULL)
         return NULL;
